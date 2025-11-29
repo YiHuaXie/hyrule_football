79f7dd21088e2b87ff711f66c000c4b4fdd6c680
NeroXie <xyh30902@163.com>
2025-11-29 08:57:42 +0800

    使用MCP 替换原有的tools 写法

--- hyrule_football/agents/hyrule_agent.py
+++ hyrule_football/agents/hyrule_agent.py
@@ -9,13 +9,6 @@ from hyrule_football.utils import get_logger
 from hyrule_football.prompts.hyrule_prompt import HyrulePrompt
 from hyrule_football.memory.chat_memory import ChatMemory
 
-# from hyrule_football.tools import (
-#     get_match_list,
-#     get_match_for_team,
-#     get_match_for_matchup,
-#     get_odds_info_for_match,
-#     plan_match_odds_query,
-# )
 
 logger = get_logger(__name__)
 
@@ -33,15 +26,6 @@ class HyruleAgent:
             model=settings.DEEPSEEK_CHAT,  # os.environ.get("DEEPSEEK_CHAT"),
             api_key=settings.DEEPSEEK_API_KEY,  # os.environ.get("DEEPSEEK_API_KEY"),
         )
-
-        # self.tools = [
-        #     get_match_list,
-        #     get_match_for_team,
-        #     get_match_for_matchup,
-        #     get_odds_info_for_match,
-        #     plan_match_odds_query,
-        # ]
-
         self.prompt = HyrulePrompt().prompt_structure()
         self.chat_memory = ChatMemory()
 
@@ -59,21 +43,6 @@ class HyruleAgent:
                 },
             }
         )
-
-        # # 创建 Agent（单智能体）
-        # self.agent = create_agent(
-        #     model=self.llm,
-        #     tools=self.tools,
-        #     system_prompt=self.prompt,
-        #     debug=True,
-        # )
-
-        # self.agent_with_memory = RunnableWithMessageHistory(
-        #     self.agent,
-        #     get_session_history=self.chat_memory.get_history,
-        #     input_messages_key="messages",
-        #     history_messages_key="messages",
-        # )
 
     async def _create_agent(self):
         """创建 Agent"""
